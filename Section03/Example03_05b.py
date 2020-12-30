a12b9a580d7a1989636c56560d2aec1a881d950b
liulawsi <liulawsi@gmail.com>
2020-12-30 17:23:14 +0800

    2020/12/30

---
+++ Section03/Example03_05b.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    單元：03_流程控制(分支)
+    Python 簡易程式範例
+    Example03_05b
+"""
+
+while True:
+    season = eval(input("請輸入月份="))
+
+    if season >0 and season < 13:
+        if season <= 3:
+            print(season, "月是春季")
+        elif season <= 6:
+            print(season, "月是夏季")
+        elif season <= 9:
+            print(season, "月是秋季")
+        elif season <= 12:
+            print(season, "月是冬季")
+    else:
+        print("輸入錯誤！")
+# end of while True     
