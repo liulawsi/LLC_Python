05f2d21662cc8816681c288a82642b60278de234
liulawsi <liulawsi@gmail.com>
2021-01-05 14:26:35 +0800

    2021/1/5

--- Section04/Example04_00.py
+++ Section04/Example04_00.py
@@ -7,7 +7,7 @@
 
 Player=["Lebrom James", 1984, 206, 113, 4,]
 
-Player[3] = Player[3] + 2
+
 
 print("姓名：",Player[0])
 print("出生：",Player[1], "年")
@@ -15,6 +15,7 @@ print("身高：",Player[2], "公分")
 print("體重：",Player[3], "公斤")
 print("NBA總冠軍：",Player[4], "次")
 
+Player[3] = Player[3] + 2
 
 scores=[23,25,19,31,20,27,]
 
--- Section04/Example04_02.py
+++ Section04/Example04_02.py
@@ -2,7 +2,7 @@
 """
     單元：04_串列(List)一定要認識
     Python 簡易程式範例
-    Example04_01
+    Example04_02
 """
 
 nums=[]
@@ -13,5 +13,9 @@ nums.append(int(input("請輸入數字3=")))
 nums.append(int(input("請輸入數字4=")))
 nums.append(int(input("請輸入數字5=")))
 
-print(nums)
+mySum= sum(nums)
+avg = mySum / 5
+print("=================")
+print("總和=", mySum)
+print("平均=", avg)
 
